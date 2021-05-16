5d9aa10e02ff71ced98460a25e09fe8bd7ee0404
Jsmond2016 <Jsmond2016@gmail.com>
2021-05-16 19:40:40 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">首页</router-link> |
+    <router-link to="/">首页11</router-link> |
     <router-link to="/about">关于</router-link>
   </div>
   <router-view />
